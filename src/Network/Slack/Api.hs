7da2d1225dc1fe926eb5a9e7e179907a1312fefe
Owain Lewis <owain@owainlewis.com>
2015-03-08 19:44:02 +0000

    minor updatse

--- src/Network/Slack/Api.hs
+++ src/Network/Slack/Api.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 module Network.Slack.Api where
 
+import           Control.Applicative     ((<$>))
 import qualified Data.ByteString.Char8   as B
 import qualified Data.ByteString.Lazy    as L
+import qualified Data.Map                as M
 import           Data.Monoid             (mconcat, (<>))
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
@@ -12,10 +13,8 @@ makeRequest :: String -> String
 makeRequest resource = mconcat [base, "/", resource]
     where base = "https://slack.com/api"
 
-chatPostMessage = makeRequest "chat.postMessage"
-
 type Endpoint = String
-type Token = String
+type Token    = String
 
 postRequest :: Endpoint -> Token -> [(B.ByteString, B.ByteString)] -> IO (Response L.ByteString)
 postRequest url token bodyParams = do
@@ -35,4 +34,29 @@ mapTuple f (x,y) = (f x, f y)
 packParams :: [(String, String)] -> [(B.ByteString, B.ByteString)]
 packParams = map (mapTuple B.pack)
 
-main token = postRequest chatPostMessage token (packParams [("channel", "#general"), ("message", "OH HELLO")])
+endpoints :: M.Map String String
+endpoints = M.fromList
+    [ ("api.test", "Checks API calling code")
+    , ("auth.test", "Checks authentication & identity")
+    , ("chat.delete", "Deletes a message")
+    , ("chat.postMessage", "Sends a message to a channel")
+    , ("chat.update", "Updates a message")
+    ]
+
+data SlackResponse = Success L.ByteString | InvalidEndpoint
+  deriving ( Show, Eq )
+
+-- Run a request and return the body
+--
+runRequest :: Token -> String -> [(String, String)] -> IO L.ByteString
+runRequest token endpoint params = postWithBody (makeRequest endpoint) token (packParams params)
+
+-- Perform a HTTP request to Slack and get a response
+--
+slackRequest :: Token -> String -> [(String, String)] -> IO SlackResponse
+slackRequest token endpoint params =
+  case M.lookup endpoint endpoints of
+    Just _ -> do
+      response <- runRequest token endpoint params
+      return . Success $ response
+    Nothing -> return InvalidEndpoint
