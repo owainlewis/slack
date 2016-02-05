30a4c5c6a3ad6c7269ff09b91b92fa916a9e8f59
Owain Lewis <owain@owainlewis.com>
2016-02-05 17:36:15 +0000

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -15,5 +15,3 @@ import qualified Network.Slack.Api as Slack
 
 sendMessageExample token channel message =
   Slack.request token "chat.postMessage" [("channel", channel), ("text", message)]
-
--- sendMessageExample "YOURTOKEN" "#general" "HELLO"
