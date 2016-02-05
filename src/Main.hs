13dd63ea017896817326f12f4e5a84dc2b0b12d4
Owain Lewis <owain@owainlewis.com>
2016-02-05 17:36:36 +0000

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-
------------------------------------------------------------------------------
---
--- SLACK
---
--- Owain Lewis owain@owainlewis.com
--- A Haskell Client for the Slack Web HTTP API
---
------------------------------------------------------------------------------
-
 module Main where
 
 import qualified Network.Slack.Api as Slack
-
-sendMessageExample token channel message =
-  Slack.request token "chat.postMessage" [("channel", channel), ("text", message)]
