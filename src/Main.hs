d392e1355ee0f02c86b5874c4d12e9f9b39a1f93
Owain Lewis <owain@owainlewis.com>
2016-05-23 09:14:19 +0100

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,38 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
 
 import qualified Network.Slack.Api as Slack
+import Options.Applicative 
+import Control.Applicative
+import Network.HTTP.Types.URI (parseQuery)
+import qualified Data.ByteString.Char8 as B
 
-main = undefined
+data Options = Options {
+    token :: String
+  , endpoint :: String
+  , params :: [(String, String)]
+  } deriving (Show, Eq)
+
+parseOptions :: Parser Options
+parseOptions = Options 
+    <$> strOption (long "token" <> short 't' <> metavar "TOKEN" <> help "Auth token")
+    <*> strArgument (metavar "ENDPOINT" <> help "endpoint, e.g. channels.list")
+    <*> (parseParams <$>
+        (strArgument (metavar "PARAMS" <> help "API request params in request query format")))
+
+parseParams :: String -> [(String, String)]
+parseParams xs = 
+    let xs' = parseQuery $ B.pack xs
+    in [(B.unpack a, B.unpack b) | (a,Just b) <- xs']
+
+opts = info (helper <*> parseOptions)
+            (fullDesc 
+             <> progDesc "Probe the Slack API"
+             <> header "slack-generic-client")
+
+main :: IO ()
+main = do
+    Options{..} <- execParser opts
+    r <- Slack.request token endpoint params
+    print r
+    putStrLn "Done"
